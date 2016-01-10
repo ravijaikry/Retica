a5a1a83ef2defa9eefbe43f1cd90a47534da090d
ravi <rshri@gmail.com>
2016-01-10 16:42:28 +0530

    added basic index page for ng

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Receta
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
